8219e4cbef3f433e0044b5163a28d1337a70b7c3
Aleksandr Vasilchuk <mailforexist@yandex.ru>
2017-03-14 15:53:51 +0300

    Add files via upload

---
+++ DZ/src/Task_1_2.java
@@ -0,0 +1,21 @@
+
+public class Task_1_2 {
+
+
+    public static void main(String[] args) {
+
+        int n = 99;
+        int num = 0;
+        int i;
+        for(i=0; i<(n/10); i++){
+            num = n % 10;
+
+        }
+
+
+
+
+        System.out.println(i+num);
+
+    }
+}
