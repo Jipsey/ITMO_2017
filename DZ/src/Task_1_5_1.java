86bbfba03f1f553e6a14467d30b849f973567c19
Aleksandr Vasilchuk <mailforexist@yandex.ru>
2017-03-15 11:31:10 +0300

    Домашнее задание №1

--- DZ/src/Task_1_5_1.java
+++ DZ/src/Task_1_5_1.java
@@ -12,9 +12,9 @@ public class Task_1_5_1 {
         System.out.print("Введите целое число: ");
         Scanner num = new Scanner(System.in);
         int a = num.nextInt();
-        String b = Integer.toBinaryString(a);
-        String f = b.substring(b.length()-1);
-        int m = Integer.parseInt(f);
+        String b = Integer.toBinaryString(a);  // переводим целое число в бинарный код и записываем в переменную b
+        String f = b.substring(b.length()-1); // записываем в переменную f часть строки
+        int m = Integer.parseInt(f);          // перевод String в int
 
         System.out.println(b + " - Бинарный код веденного числа");
         System.out.println(m + " - Последняя цифра бинарного кода");
