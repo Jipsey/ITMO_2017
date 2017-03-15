822fa53a1348fe0ab35b98ebc7935d5cf3d3102a
Aleksandr Vasilchuk <mailforexist@yandex.ru>
2017-03-15 03:32:04 +0300

    Домашнее задание №1

---
+++ DZ/src/Task_1_3.java
@@ -0,0 +1,10 @@
+
+public class Task_1_3 {
+
+    public static void main(String[] args) {
+        double n = 7.65;
+        int num = (int)Math.round(n);
+        System.out.println(num);
+    }
+
+}
---
+++ DZ/src/Task_1_4.java
@@ -0,0 +1,31 @@
+
+public class Task_1_4 {
+
+    public static void main(String[] args) {
+
+        int a = 678;
+        int i=0;
+        int n=0;
+        int num1 = 0;
+        int num2 = 0;
+
+           if (a>100 & a<1000){
+            for(i=0;i<(a/100);i++) {
+                num1 = a % 100;
+                }
+
+                if (num1>10 & num1<100){
+                   for (n = 0; n < (num1/10); n++) {
+                    num2 = num1 % 10;
+                    }
+                }
+
+            }
+
+        System.out.println(i + n + num2);
+    }
+        }
+
+
+
+
