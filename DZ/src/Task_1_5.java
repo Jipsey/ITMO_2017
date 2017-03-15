e76d23e34d3ae9873cd3577c5f1d33184e1a5e51
Aleksandr Vasilchuk <mailforexist@yandex.ru>
2017-03-15 03:59:01 +0300

    Домашнее задание №1

---
+++ DZ/src/Task_1_5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task_1_5 {
+
+    public static void main(String[] args) {
+        System.out.print("Введите любое число: ");
+        Scanner num = new Scanner(System.in);
+        int a = num.nextInt();
+        int b = a % 2;
+
+        System.out.print(b!=1);
+        if (b!=1){
+            System.out.println(" - Вы ввели чётное число");
+        }
+        else System.out.println(" - Вы ввели нечётное число");
+    }
+}
