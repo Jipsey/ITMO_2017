a13e87b9b52828553326d99db173d63ec34fe72f
Aleksandr Vasilchuk <mailforexist@yandex.ru>
2017-03-15 11:07:38 +0300

    Домашнее задание №1

--- DZ/src/Task_1_5_1.java
+++ DZ/src/Task_1_5_1.java
@@ -1,3 +1,8 @@
+/*
+программа определяющая четное число ввел
+пользователь или нечетное, зная что все четные числа в двоичной системе исчисления оканчиваются на 0.
+ */
+
 import java.util.Scanner;
 
 public class Task_1_5_1 {
@@ -6,13 +11,16 @@ public class Task_1_5_1 {
         System.out.print("Введите целое число: ");
         Scanner num = new Scanner(System.in);
         int a = num.nextInt();
-        int b = 2;
-        int x= a | b;
-        System.out.println(x);
-        //System.out.print(b!=1);
-        //if (b!=1){
-        //    System.out.println(" - Вы ввели чётное число");
-        //}
-        //else System.out.println(" - Вы ввели нечётное число");
+        String b = Integer.toBinaryString(a);
+        String f = b.substring(b.length()-1);
+        int m = Integer.parseInt(f);
+
+        System.out.println(b + " - Бинарный код веденного числа");
+        System.out.println(m + " - Последняя цифра бинарного кода");
+        if (m != 1){
+            System.out.println("Вы ввели чётное число");
+        }
+        else System.out.println("Вы ввели нечётное число");
+
     }
 }
