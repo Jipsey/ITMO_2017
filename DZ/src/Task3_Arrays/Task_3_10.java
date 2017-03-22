6cb86f4750716393f2d454dc16a25e958d9edd36
Aleksandr Vasilchuk <mailforexist@yandex.ru>
2017-03-22 09:16:46 +0300

    Домашнее задание №3 Массивы

---
+++ DZ/src/Task3_Arrays/Task_3_10.java
@@ -0,0 +1,35 @@
+/**
+ *   Создайте массив из 11 случайных целых чисел из отрезка [-1;1], выведите массив на
+     экран в строку. Определите какой элемент встречается в массиве чаще всего и выведите об
+     этом сообщение на экран. Если два каких-то элемента встречаются одинаковое количество
+     раз, то не выводите ничего.
+ */
+package Task3_Arrays;
+
+import java.util.Arrays;
+
+public class Task_3_10 {
+    public static void main(String[] args) {
+
+        int[] arr = new int[11];
+        int a,b, c;
+        a=b=c=0;
+
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i] = mine.rand(-1,1); }
+        System.out.println(Arrays.toString(arr) + "  массив до сортировки");
+            Arrays.sort(arr);
+
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i] == -1) {a++;}
+            else if(arr[i] == 0){b++;}
+            else c++; }
+        System.out.println(Arrays.toString(arr) + "  массив после сортировки");
+        System.out.printf("%s %s %s\n",a,b,c);
+        if ((a > b) && (a > c)){ System.out.println("Элемент '-1' встречается чаще всего");}
+        if ((b > a) && (b > c)){System.out.println("Элемент '0' встречается чаще всего");}
+        if((c > a) && (c > b)){System.out.println("Элемент '1' встречается чаще всего");}
+
+    }
+
+}
--- DZ/src/Task3_Arrays/mine.java
+++ DZ/src/Task3_Arrays/mine.java
@@ -4,7 +4,7 @@ package Task3_Arrays;
  * в данном классе созданы методы которые ниболее часто используются при решении задач
  */
 public class mine {
-    public static int rand ( int min, int max){   // метод для присвоения рандомных чисел
+    public static int rand ( int min, int max){   // метод для создания случайных чисел из заданного диапазона
         max = max - min;
         return (int) (Math.random() * ++max) + min;
     }
